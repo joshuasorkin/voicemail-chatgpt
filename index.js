6e5ed22e2c863e7ae9371b2faa4c0abae8dc51f4
joshua sorkin <joshua.d.sorkin@gmail.com>
2023-08-31 13:19:34 -0700

    add check for no user messages

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post('/twilio-webhook', async (req, res) => {
         speechTimeout:process.env.TWILIO_SPEECH_TIMEOUT_SECONDS
     });
     let greeting = "What would you like to say?";
-    if(!callsData[callSid]){
+    if(!callsData[callSid] || callsData[callSid].userMessages.length === 0){
         greeting = "Hi!  I'm Chat GPT.  " + greeting;
     }
     twimlBuilder.sayReading(gather,"Hi!  I'm Chat GPT. What would you like to say?");
