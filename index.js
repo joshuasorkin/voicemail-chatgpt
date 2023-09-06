1a252e3d7df4b2b6edd3be979f267eb89b32ad21
joshua sorkin <joshua.d.sorkin@gmail.com>
2023-09-06 08:10:19 -0700

    add comma

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function chatGPTGenerate(userMessages) {
         const messages = [
             {role: 'system', content: 'Your response must be 2000 characters or less.'},
             {role: 'system', content: 'Use casual, conversational American English.'},
-            {role: 'system', content: 'If I do not ask for a specific length of response, limit your response to 3 sentences or less.'}
+            {role: 'system', content: 'If I do not ask for a specific length of response, limit your response to 3 sentences or less.'},
             {role: 'system', content: 'At the end of your response, ask me an open-ended question related to our discussion.'}
         ]
         userMessages.forEach(message => {
