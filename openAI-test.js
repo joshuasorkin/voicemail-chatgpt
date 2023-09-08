7f298d190e921f741e0c5e8645ba1cad117b1772
joshua sorkin <joshua.d.sorkin@gmail.com>
2023-09-08 08:48:33 -0700

    add refactored openAI files to tracking

--- openAI-test.js
+++ openAI-test.js
@@ -1,4 +1,4 @@
-import OpenAIUtil from './openAI-util';
+import OpenAIUtil from './openAI-util.js';
 const openAIUtil = new OpenAIUtil();
 const response = openAI.chatGPTGenerate([{role:'user',content:'I am testing your API.'}]);
 console.log(response);
