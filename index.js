d69648f516ca8351cdee95e0bb96f8ebbbcdf232
joshua sorkin <joshua.d.sorkin@gmail.com>
2023-09-09 18:34:57 -0700

    import firebase db

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//todo: decouple index.js from firebase by separating callsData operations into its own callsData.js class
+//and a DatabaseController class that gets extended as a DatabaseController_Firebase class,
+//as suggested by chatGPT here: https://chat.openai.com/share/f0a4417b-481a-462a-8662-5d23434e11e0
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -7,7 +11,7 @@ import bodyParser from 'body-parser';
 import twilio from 'twilio';
 import OpenAI from 'openai';
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 
 //local file imports
 import VoiceResponse from 'twilio/lib/twiml/VoiceResponse.js';
