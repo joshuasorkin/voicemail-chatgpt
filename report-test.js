c1b9bba0004a54d40a8aa2a136001e34992bc4da
joshua sorkin <joshua.d.sorkin@gmail.com>
2023-09-18 11:07:09 -0700

    write getPhoneList test

--- report-test.js
+++ report-test.js
@@ -3,5 +3,23 @@ dotenv.config();
 
 import Database from './Database.js';
 import Report from './Report.js';
+import twilio from 'twilio';
 
 const database = new Database();
+
+// Twilio configuration
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const report = new Report(database,client);
+
+async function getPhoneList_test(){
+    const phoneList = await report.getPhoneList();
+    for (const phone of phoneList) {
+        console.log(phone);
+    }
+}
+
+async function test(){
+    await getPhoneList_test();
+}
+
+test();
