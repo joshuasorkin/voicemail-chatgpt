7d1d68127648a726f4931f70251541c988f39ec9
joshua sorkin <joshua.d.sorkin@gmail.com>
2023-09-18 11:00:45 -0700

    getphonelist

--- Report.js
+++ Report.js
@@ -1,10 +1,23 @@
-class Report{
-    constructor(database){
+class Report {
+    constructor(database, client) {
         this.database = database;
+        this.client = client;
     }
 
-    async getPhoneList(){
-        const callSids = await database.getAllCallSids();
-        
+    async getPhoneList() {
+        const phoneList = new Set();
+        const callSids = await this.database.getAllCallSids();
+
+        for (const callSid of callSids) {
+            const call = await this.client.calls(callSid).fetch();
+            const from = call.from;
+            if (!phoneList.has(from)) {
+                phoneList.add(from);
+            }
+        }
+
+        return phoneList;
     }
 }
+
+export default Report;
