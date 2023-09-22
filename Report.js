a0d5b36e06a79bb65faf17b84a11b4d55f95238e
joshua sorkin <joshua.d.sorkin@gmail.com>
2023-09-22 11:36:33 -0700

    move getAllCallSids into Report

--- Report.js
+++ Report.js
@@ -4,6 +4,17 @@ class Report {
         this.client = client;
     }
 
+    async getAllCallSids(){
+        const cursor = await this.calls.find();
+        const callSidArray = [];
+        while (await cursor.hasNext()) {
+            const document = await cursor.next();
+            const callSid = document.callSid;
+            callSidArray.push(callSid);
+        }
+        return callSidArray;
+    }
+
     async getPhoneList() {
         const phoneList = new Set();
         const callSids = await this.database.getAllCallSids();
