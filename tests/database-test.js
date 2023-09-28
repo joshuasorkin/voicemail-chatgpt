7a0a2fdbc9310c2e4d226019bc647b1f7411cd23
joshua sorkin <joshua.d.sorkin@gmail.com>
2023-09-28 14:35:43 -0700

    remove released twilio number from personalityCache-test

--- tests/database-test.js
+++ tests/database-test.js
@@ -29,76 +29,13 @@ async function reset_test(){
     await database.resetCollection("test");
     console.log("collection reset");
 }
-async function getCall_test(){
-console.log(`checking for call ${callSid}...`);
-    const call = await database.getCall(callSid);
-    if (call){
-        console.log("call exists: ",call);
-    }
-    else{
-        console.log("call does not exist");
-    }
-}
-
-async function getOrAddCall_test(){
-    let call = await database.getCall(callSid_getOrAdd);
-    if (call){
-        console.log("call exists: ",call);
-    }
-    else{
-        console.log(`call ${callSid_getOrAdd} does not exist`);
-    }
-    call = await database.getOrAddCall(callSid_getOrAdd);
-    console.log(call);
-}
-
-async function addCall_test(){
-    const result = await database.addCall(callSid);
-    console.log(`Inserted call with doc ID ${result.insertedId}`);
-}
-
-async function addUserMessage_test(){
-    const result = await database.addUserMessage(callSid,userMessage);
-    console.log(
-        `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
-    );
-}
-
-async function addAssistantMessage_test(){
-    const result = await database.addAssistantMessage(callSid,assistantMessage);
-    console.log(
-        `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
-    );
-}
-
-async function getUserMessages_test(){
-    const result = await database.getUserMessages(callSid);
-    console.log("userMessages: ",result);
-}
-
-async function getStreamSid_test(){
-    const result = await database.getStreamSid(callSid);
-    console.log("streamSid: ",result);
-}
-async function setStreamSid_test(){
-    const result = await database.setValue(callSid,"streamSid",streamSid);
-    console.log(`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`);
-}
 
 async function getValue_test(){
     const result = await database.getValue(callSid,valueName);
     console.log(`${valueName}: ${result}`);
 }
 
-async function getAllPersonalities_test(){
-    const result = await database.getAllPersonalities();
-    console.log(result["standard"]);
-}
 
-async function getPersonalityNameFromPhoneNumber_test(){
-    const result = await database.getPersonalityNameFromPhoneNumber("+15107564445");
-    console.log(result);
-}
 
 async function getAllCallSids_test(){
     const result = await database.getAllCallSids();
