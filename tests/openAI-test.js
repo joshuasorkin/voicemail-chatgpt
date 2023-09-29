2250ec4d30848e9b17abc249338286935746f60e
joshua sorkin <joshua.d.sorkin@gmail.com>
2023-09-29 13:02:02 -0700

    remove firebase database reference from database-test

--- tests/openAI-test.js
+++
@@ -1,6 +0,0 @@
-import OpenAIUtil from './openAI-util.js';
-import dotenv from 'dotenv';
-dotenv.config();
-const openAIUtil = new OpenAIUtil();
-const response = openAIUtil.chatGPTGenerate([{role:'user',content:'I am testing your API.'}]);
-console.log(response);
