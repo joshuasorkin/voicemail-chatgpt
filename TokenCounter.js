19a78eeafd4be293cc87cc18528415f7b711bbe5
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-10-07 20:22:51 -0700

    add test strings

--- TokenCounter.js
+++ TokenCounter.js
@@ -5,8 +5,17 @@ class TokenCounter{
     constructor(){
         this.enc = get_encoding('cl100k_base');
     }
+    
     encode(str){
         return this.enc.encode(str);
+    }
+
+    analyzeUserMessages(call){
+        const analysis = [];
+        call.userMessages.forEach(msg => {
+            analysis.push(this.enc.encode(msg.content).length);
+
+        })
     }
 }
 
--- test/tokencounter-test.js
+++ test/tokencounter-test.js
@@ -1,6 +1,7 @@
 import TokenCounter from '../TokenCounter.js';
 
-const testString = "For a number of years now, work has been proceeding in order to bring perfection to the crudely conceived idea of a transmission that would not only supply inverse reactive current for use in unilateral phase detractors, but would also be capable of automatically synchronizing cardinal grammeters."
+const testString1 = "For a number of years now, work has been proceeding in order to bring perfection";
+const testString2 = "to the crudely conceived idea of a transmission that would not only supply inverse reactive current for use in unilateral phase detractors, but would also be capable of automatically synchronizing cardinal grammeters."
 
 const tokenCounter = new TokenCounter();
 
